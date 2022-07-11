f351c1a8ec9e67346aaa0cb9ecaafd696047cb07
lariskovski <larissaporto@live.com>
2022-07-10 21:21:58 -0300

    Add grid to series image display

---
+++ src/components/Grid/Grid.styles.js
@@ -0,0 +1,18 @@
+import { createUseStyles } from 'react-jss';
+
+export default createUseStyles({
+  grid: {
+    display: 'grid',
+    gridTemplateRows: 'repeat(2, 1fr)',
+    gridTemplateColumns: 'repeat(5, 1fr)',
+    // padding: '1%',
+    // width: '80vw',
+    // height: '50vh',
+    // outline: '2px solid black'
+},
+
+  // cell:{
+  //     outline: '2px solid black',
+  //     padding: '10%'
+  //   }
+});
--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -2,30 +2,37 @@ import React from 'react';
 import './index.css';
 import { Link } from 'react-router-dom';
 import Default from '../Default';
-
+import useStyles from '../Grid/Grid.styles';
 
 const SeriesListItem = ({ series }) => (
-    <li className='show-item'>
+    // <li className='list-item'>
+    <div style={{ padding: 20}}>
+
             <Link to={'series/' + series.show.id}>
-                {
-                    (series.show.image != null
-                    &&
-                    <div><img alt="Show icon" src={(series.show.image.medium)}/></div>)
-                    ||
-                    <Default />
-                }
-                {series.show.name}
+                    {
+                        (series.show.image != null
+                            &&
+                            <div>
+                            <img alt="Show icon" src={(series.show.image.medium)}/>
+                        </div>
+                        )
+                        ||
+                        <Default />
+                    }
+                    {series.show.name}
             </Link>
-    </li>
+    </div>
+    // </li>
 )
 const SeriesList = (props) => {
+    const classes = useStyles();
     return (
-        <div>
-            <ul className='series-list'>
+        <div className={classes.grid}>
+            {/* <ul className='list'> */}
                 {props.list.map(series => (
                     <SeriesListItem series={series} key={series.show.id}/>
                 ))}
-            </ul>
+            {/* </ul> */}
         </div>
 );
 }
